497d9af7382daf4c11e0cbdf3c6ed5a4a0c2e8e3
Rohit Kumar <rohit.kumar@xynotech.com>
2022-12-23 15:15:51 +0500

    Test Cases

---
+++ TestXCouldApp/MathStuff.swift
@@ -0,0 +1,27 @@
+//
+//  MathStuff.swift
+//  TestXCouldApp
+//
+//  Created by Rohit Kumar on 12/23/22.
+//
+
+import Foundation
+
+class MathStuff {
+    
+    func add(x: Int, y: Int) -> Int {
+        return x + y
+    }
+    
+    func subtract(x: Int, y: Int) -> Int {
+        return x - y
+    }
+    
+    func multiply(x: Int, y: Int) -> Int {
+        return x * y
+    }
+    
+    func divide(x: Int, y: Int) -> Int {
+        return x / y
+    }
+}
--- TestXCouldAppTests/TestXCouldAppTests.swift
+++ TestXCouldAppTests/TestXCouldAppTests.swift
@@ -10,27 +10,28 @@ import XCTest
 
 final class TestXCouldAppTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testAddStuff() {
+        let math = MathStuff()
+        let result = math.add(x: 1, y: 5)
+        XCTAssertEqual(result, 6)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testSubtractStuff() {
+        let math = MathStuff()
+        let result = math.subtract(x: 10, y: 5)
+        XCTAssertEqual(result, 5)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testMultiplyStuff() {
+        let math = MathStuff()
+        let result = math.multiply(x: 2, y: 5)
+        XCTAssertEqual(result, 10)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testDivideStuff() {
+        let math = MathStuff()
+        let result = math.divide(x: 15, y: 5)
+        XCTAssertEqual(result, 3)
     }
-
+   
 }
